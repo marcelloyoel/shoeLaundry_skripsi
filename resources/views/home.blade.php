fafc581314a034116a4bd7f5e70b757734712617
marcelloyoel <marcelloyoell@gmail.com>
2023-07-20 20:57:18 +0700

    fixing template again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,26 @@
-@extends('layouts.main')
+@extends('partials.bodyPages')
 @section('sidebar')
     @include('partials.sidebar')
 @endsection
 @section('container')
     <h1>Hello World!</h1>
+    <p>Ini cuman mau ngetest aja sbenernya bisa ga sih bikin begini</p>
+
+    <div class="row">
+        @for ($i = 0; $i < 25; $i++)
+            <div class="col-sm-4">
+                <div class="card mt-5" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Test Card ke-{{ $i }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's
+                            content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        @endfor
+    </div>
 @endsection
--- resources/views/nonlogin/login.blade.php
+++ resources/views/nonlogin/login.blade.php
@@ -89,14 +89,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/template/vendor/jquery/jquery.min.js"></script>
+    <script src="/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="/template/js/sb-admin-2.min.js"></script>
 
 </body>
 
