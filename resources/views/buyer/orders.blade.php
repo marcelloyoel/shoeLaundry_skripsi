f66bdc599b6d4721327cef4431893d9e20aed679
Jonathan Ernest <jonathanernest9029@gmail.com>
2023-09-24 18:51:31 +0700

    Ngapus tulisan plugin

--- resources/views/buyer/orders.blade.php
+++ resources/views/buyer/orders.blade.php
@@ -5,10 +5,6 @@
 @section('container')
     <!-- Page Heading -->
     <h1 class="h3 mb-2 text-gray-800">Order List</h1>
-    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-        For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official
-            DataTables documentation</a>.</p>
-
     @if (session()->has('update'))
         <div class="alert alert-success" role="alert">
             {{ session('update') }}
