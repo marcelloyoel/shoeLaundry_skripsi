d1593507c80b9c2c506f91cdf1c601f39cb24405
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-10-15 23:26:04 +0700

    Add profile buyer and laundry finish

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\LaundrySepatu;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -44,35 +45,56 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
+        $rules = [
             'username' => ['unique:users', 'required'],
             'displayName' => ['required'],
             'address' => ['nullable'],
             'email' => ['unique:users', 'required'],
             'phoneNumber' => ['nullable'],
-            'status'    => ['required'],
-            'group_id'  => ['required'],
-            'picture' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
+            'group_id' => ['required'],
+            'password' => 'required',
+            'picture' => 'image',
+        ];
+
+        if ($request->input('group_id') == 2) {
+            $rules['laundry-laundrySepatuName'] = ['required'];
+            $rules['laundry-laundrySepatuSlug'] = ['required'];
+            $rules['laundry-bio'] = ['required'];
+            $rules['laundry-address'] = ['required'];
+            $rules['laundry-contact'] = ['required'];
+            $rules['laundry-picture'] = ['nullable'];
+        }
+
+        $validatedData = $request->validate($rules);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
 
         if ($request->hasFile('picture')) {
             $uploadedPicture = $request->file('picture');
             $pictureFileName = $uploadedPicture->getClientOriginalName();
             $uploadedPicture->storeAs('images', $pictureFileName, 'public');
-            $validatedData['picture'] = $pictureFileName; // Store the filename in the 'picture' field
+            $validatedData['picture'] = $pictureFileName;
         }
 
-        $validatedData['password'] = bcrypt('12345');
-        // dd($validatedData);
-
-        // User::create($validatedData);
         $user = User::create($validatedData);
 
         if ($user->group_id == 2) {
             $laundryData = [
-                'user_id' => $user->id, // Associate the user with the laundry
-                // Add any other relevant data for the laundry
+                'user_id' => $user->id,
+                'laundrySepatuName' => $validatedData['laundry-laundrySepatuName'],
+                'laundrySepatuSlug' => $validatedData['laundry-laundrySepatuSlug'],
+                'bio' => $validatedData['laundry-bio'],
+                'Address' => $validatedData['laundry-address'],
+                'Contact' => $validatedData['laundry-contact'],
+                'distance' => '200 m',
             ];
+
+            if ($request->hasFile('laundry-picture')) {
+                $uploadedLaundryPicture = $request->file('laundry-picture');
+                $laundryPictureFileName = $uploadedLaundryPicture->getClientOriginalName();
+                $uploadedLaundryPicture->storeAs('images', $laundryPictureFileName, 'public');
+                $laundryData['picture'] = $laundryPictureFileName;
+            }
 
             LaundrySepatu::create($laundryData);
         }
