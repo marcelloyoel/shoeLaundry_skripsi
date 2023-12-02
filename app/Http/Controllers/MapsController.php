78519126be7e5120add123efbe09146e9b7a735c
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-02 15:15:30 +0700

    use $_SERVER['REMOTE_ADDR'];

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -35,9 +35,12 @@ class MapsController extends Controller
 
     public function calculateDistance(Request $request)
     {
-        $ip = $request->ip();
+        // $ip = $request->ip();
+        $ip = $_SERVER['REMOTE_ADDR'];
         // $ip = '49.35.41.195'; // contoh ip address public
         $currentUserInfo = Location::get($ip);
+
+        // dd($currentUserInfo->ip);
 
         // $users = User::all();
         $user = auth()->user();
