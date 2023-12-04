410e19f4c01efd2c54418c59c26151e1b68466d0
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-04 23:39:24 +0700

    remove new code

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -41,10 +41,6 @@ class MapsController extends Controller
         // $ip = '49.35.41.195'; // contoh ip address public
         $currentUserInfo = Location::get($ip);
 
-        $url = "http://ip-api.com/" . $ip;
-        $response = Http::get($url);
-        $data = $response->json();
-
         // $users = User::all();
         $user = auth()->user();
         if ($user) {
@@ -62,7 +58,7 @@ class MapsController extends Controller
             // echo "User not authenticated";
         }
 
-        dd($ip, $currentUserInfo, $currentUserInfo->longitude, $currentUserInfo->latitude, $origin, $data);
+        dd($ip, $currentUserInfo, $currentUserInfo->longitude, $currentUserInfo->latitude, $origin);
 
         // $origin = 'Jl. Pintu Air Raya No.2-F, RT.7/RW.1, Ps. Baru, Kecamatan Sawah Besar, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10710';
         // $origin = 'Jl. Jalur Sutera Bar. No.Kav. 21, RT.001/RW.004, Panunggangan, Kec. Pinang, Kota Tangerang, Banten 15143';
