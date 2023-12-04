76f5cb6797782ff960776e0c0de970ce8d248db4
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-04 22:18:28 +0700

    change to default

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -35,7 +35,7 @@ class MapsController extends Controller
 
     public function calculateDistance(Request $request)
     {
-        $ip = $request->getRemoteAddr();
+        $ip = $request->ip();
         // $ip = $_SERVER['REMOTE_ADDR'];
         // $ip = '49.35.41.195'; // contoh ip address public
         $currentUserInfo = Location::get($ip);
