d0e3f5e1bb8835cf70d9098663e629989da37191
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-07 22:20:53 +0700

    add alert

--- public/js/home.js
+++ public/js/home.js
@@ -22,16 +22,17 @@ function showPosition(position) {
         type: 'POST',
         url: "/getLocation",
         data: {
-            _token:         csrf,
+            // _token:         csrf,
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         },
         success: function(ajax) {
-            console.log("BERHASIL");
             console.log($.ajax);
+            alert("Berhasil");
         },
         error: function(request, error) {
             console.log(error);
+            alert("Gagal");
         }
     });
 }
