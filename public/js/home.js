80394e6d96d33dd664f3e28633082341a606aa5b
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-07 13:52:53 +0700

    test debug long lat

--- public/js/home.js
+++ public/js/home.js
@@ -12,7 +12,9 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    console.log("TEST");
+    console.log(position);
+    console.log("Latitude: " + position.coords.latitude);
+    console.log("Longitude: " + position.coords.longitude);
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
