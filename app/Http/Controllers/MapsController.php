ce156ef048a8e674ac989bc92d2bf59e10663256
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-08 09:32:00 +0700

    change to decode

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -43,10 +43,8 @@ class MapsController extends Controller
         // $currentUserInfo = Location::get($ip);
 
         // Get the latitude and longitude from the request
-        $latitude = $request->input('latitude');
-        $longitude = $request->input('longitude');
-        $latitude = json_encode($latitude);
-        $longitude = json_encode($longitude);
+        $latitude = json_decode($request->input('latitude'));
+        $longitude = json_decode($request->input('longitude'));
 
         dd($latitude, $longitude);
 
