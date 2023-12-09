e19ff2cc3a8f1e13d56d4589c93b71840b5ee385
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-09 16:11:22 +0700

    try to use user address

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -58,7 +58,7 @@ class MapsController extends Controller
             // } else {
             // if ip address is public
             // $origin = session('temp_address') ?? $currentUserInfo->latitude . ', ' . $currentUserInfo->longitude;
-            $origin = session('temp_address') ?? $latitude . ', ' . $longitude;
+            $origin = session('temp_address') ?? $user->address;
             // $origin = 'hypermart villa melati mas';
             // }
 
