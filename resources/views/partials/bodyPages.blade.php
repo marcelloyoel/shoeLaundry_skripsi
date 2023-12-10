5807c79e4251b2f8e10966b21d687f8dfa1a84ea
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-10 18:57:22 +0700

    move script

--- resources/views/partials/bodyPages.blade.php
+++ resources/views/partials/bodyPages.blade.php
@@ -196,6 +196,61 @@
         let table = new DataTable('#myTable');
     </script>
 
+    {{-- Home Script Location --}}
+    <script>
+        console.log('home js');
+        const x = document.getElementById("demo");
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            console.log(position);
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            console.log("Coords User: " + latitude + ',' + longitude);
+
+            var csrf = $("#csrf-token").val();
+
+            // Create a JavaScript object with latitude and longitude
+            var locationData = {
+                _token: csrf,
+                latitude: latitude,
+                longitude: longitude
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: "/mapsLocation",
+                // data: JSON.stringify(locationData), // Convert object to JSON string
+                // contentType: 'application/json', // Set content type for JSON
+                data: {
+                    _token:         csrf,
+                    latitude: latitude,
+                    longitude: longitude
+                },
+                success: function(ajax) {
+                    console.log($.ajax);
+                    alert("Managed to find the location");
+                },
+                error: function(request, error) {
+                    console.log(error);
+                    alert("Failed to find its location");
+                }
+            });
+        }
+
+        window.onload = function() {
+            getLocation();
+        };
+    </script>
+
     {{-- openai --}}
     {{-- <script src="/vendor/snapappointments/bootstrap-select/dist/js/bootstrap-select.min.js"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
