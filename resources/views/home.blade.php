854b943ad550e0b2587bbf0a54d71c57b08fa291
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-12 20:07:58 +0700

    finish debug alert location null

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,7 +48,7 @@
                         <h6 class="card-text">📌 : {{ Str::words($laundry->Address, 6, '...') }}</h6>
                         {{-- <h6 class="card-text">📌 : {{ $laundry->Address }}</h6> --}}
                         <h6 class="card-text">☎ : {{ $laundry->Contact }}</h6>
-                        <h6 class="card-text">📍 : {{ 'Not available' ?? $calculatedDistances[$laundry->user_id] }} </h6>
+                        <h6 class="card-text">📍 : {{ $calculatedDistances[$laundry->user_id] ?? 'Not available' }} </h6>
                         <a href="laundry/{{ $laundry->id }}" class="btn btn-primary mt-2">Laundry Detail</a>
                     </div>
                 </div>
@@ -89,15 +89,9 @@
 
     <div>
         @if($calculatedDistances[$laundry->user_id] === null)
-            {{-- <h6 class="card-text">📍 : Not available </h6>
-            <script>
-                alert("We can't find ur location, please input manually.");
-            </script> --}}
-        @else
             <script>
                 alert("We can't find ur location, please input manually.");
             </script>
-            {{-- <h6 class="card-text">📍 : {{ $calculatedDistances[$laundry->user_id] }} </h6> --}}
         @endif
     </div>
 @endsection
