37aa526bb0ec08eefa9750257d0beea64d8fac66
Leonardo472 <115207925+Leonardo472@users.noreply.github.com>
2023-12-12 20:06:13 +0700

    fix alert in home if location null

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,18 +48,7 @@
                         <h6 class="card-text">📌 : {{ Str::words($laundry->Address, 6, '...') }}</h6>
                         {{-- <h6 class="card-text">📌 : {{ $laundry->Address }}</h6> --}}
                         <h6 class="card-text">☎ : {{ $laundry->Contact }}</h6>
-                        @if($calculatedDistances[$laundry->user_id] === null)
-                            {{-- <h6 class="card-text">📍 : Not available </h6>
-                            <script>
-                                alert("We can't find ur location, please input manually.");
-                            </script> --}}
-                        @else
-                            <h6 class="card-text">📍 : Not available </h6>
-                            <script>
-                                alert("We can't find ur location, please input manually.");
-                            </script>
-                            {{-- <h6 class="card-text">📍 : {{ $calculatedDistances[$laundry->user_id] }} </h6> --}}
-                        @endif
+                        <h6 class="card-text">📍 : {{ 'Not available' ?? $calculatedDistances[$laundry->user_id] }} </h6>
                         <a href="laundry/{{ $laundry->id }}" class="btn btn-primary mt-2">Laundry Detail</a>
                     </div>
                 </div>
@@ -96,5 +85,19 @@
             </nav>
         </div>
 
+    </div>
+
+    <div>
+        @if($calculatedDistances[$laundry->user_id] === null)
+            {{-- <h6 class="card-text">📍 : Not available </h6>
+            <script>
+                alert("We can't find ur location, please input manually.");
+            </script> --}}
+        @else
+            <script>
+                alert("We can't find ur location, please input manually.");
+            </script>
+            {{-- <h6 class="card-text">📍 : {{ $calculatedDistances[$laundry->user_id] }} </h6> --}}
+        @endif
     </div>
 @endsection
