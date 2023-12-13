bf863e60049e3cb72449e8bc569dff6c5a6b719d
marcelloyoel <marcelloyoell@gmail.com>
2023-12-13 20:35:15 +0700

    ketinggalan

--- resources/views/nonlogin/login.blade.php
+++ resources/views/nonlogin/login.blade.php
@@ -97,6 +97,54 @@
     <script src="/template/js/sb-admin-2.min.js"></script>
 
     <script>
+        console.log('login js');
+        const x = document.getElementById("demo");
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            console.log(position);
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            console.log("Coords User: " + latitude + ',' + longitude);
+
+            var csrf = $("#csrf-token").val();
+
+            // Create a JavaScript object with latitude and longitude
+            var locationData = {
+                _token: csrf,
+                latitude: latitude,
+                longitude: longitude
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: "/loginLocation",
+                // data: JSON.stringify(locationData), // Convert object to JSON string
+                // contentType: 'application/json', // Set content type for JSON
+                data: {
+                    _token: csrf,
+                    latitude: latitude,
+                    longitude: longitude
+                },
+                success: function(ajax) {
+                    console.log($.ajax);
+                    // alert("Managed to find the location");
+                },
+                error: function(request, error) {
+                    console.log(error);
+                    alert("Failed to find your location");
+                }
+            });
+        }
+
         window.onload = function() {
             getLocation();
         };
