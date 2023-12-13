c57caa03859b7f0644109aee75d3dd6072b21e37
marcelloyoel <marcelloyoell@gmail.com>
2023-12-13 20:31:36 +0700

    missing script

--- resources/views/nonlogin/login.blade.php
+++ resources/views/nonlogin/login.blade.php
@@ -96,6 +96,11 @@
     <!-- Custom scripts for all pages-->
     <script src="/template/js/sb-admin-2.min.js"></script>
 
+    <script>
+        window.onload = function() {
+            getLocation();
+        };
+    </script>
 </body>
 
 </html>
