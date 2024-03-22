e7a666387d14a2335a41add21adac16e7b84c0ab
Marcello Yoel Christianus <marcelloyoell@gmail.com>
2024-03-22 18:24:12 +0700

    ketinggalan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\LaundrySepatuController;
